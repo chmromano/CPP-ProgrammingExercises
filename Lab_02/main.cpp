8e81efdf0d203affa265f89d7a6c318a558c1d66
Christopher Romano <christopher.romano@icloud.com>
2021-11-04 14:24:26 +0200

    Finished Lab02.

--- Lab_02/main.cpp
+++ Lab_02/main.cpp
@@ -25,14 +25,16 @@ int main() {
 }
 
 string find_field(const string &xml, string tag_name) {
+    // Generate start and end tags.
     string start_tag("<" + tag_name + ">"), end_tag("</" + tag_name + ">");
 
+    //Find start and end position of searched item's string.
     int start_pos = xml.find(start_tag) + start_tag.size();
     int end_pos = xml.find(end_tag);
 
     if (start_pos == string::npos || end_pos == string::npos) {
-        return "not found"; // return this if tag is not found
+        return "not found"; // Return this if tag is not found.
     } else {
-        return xml.substr(start_pos, end_pos - start_pos);
+        return xml.substr(start_pos, end_pos - start_pos); // Return substring if tag is found.
     }
 }
