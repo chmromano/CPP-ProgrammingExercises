b7398a523f69abdda190cc6f0e4ad9e98f34624b
Christopher Romano <christopher.romano@icloud.com>
2021-11-06 15:50:43 +0200

    Lab03 WIP.

--- Lab_03_A/main.cpp
+++ Lab_03_A/main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int count_digits(double tmp);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int spacing = 0;
+
+    vector<double> doubles_vect;
+    while (true) {
+        cout << "Enter a double: ";
+
+        double tmp;
+        cin >> tmp;
+
+        // Input checking.
+        if (!cin.good()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. ";
+        } else if (tmp >= 0) {
+            //Append to vector.
+            doubles_vect.push_back(tmp);
+
+            //Count the digits now.
+            int digits = count_digits(tmp) + 4; //4 to account for the '.' and 3 decimals.
+            if (digits > spacing) spacing = digits;
+        } else {
+            break;
+        }
+    }
+
+    if (doubles_vect.empty()) {
+        cout << "\nNo numbers were entered." << endl;
+    } else {
+        //Sort the vector.
+        sort(doubles_vect.begin(), doubles_vect.end());
+        //Just trying stuff out and learning. const double &i means working with the original vector element,
+        //but without being able to modify it. I assume it's good in this case since I'm just printing the value.
+        for (const double &i: doubles_vect) {
+            cout << fixed << right << setw(spacing) << setprecision(3) << i << endl;
+        }
+    }
+
     return 0;
+}
+
+//Function to count the digits in the integer-part of the double.
+int count_digits(double tmp) {
+    int digits = 0;
+    while (tmp > 1) {
+        tmp /= 10;
+        digits++;
+    }
+    return digits;
 }
