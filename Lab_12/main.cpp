f7b3df1f9221c04f07f07187c6e0d8ef007bb161
Christopher Romano <christopher.romano@icloud.com>
2021-12-16 22:35:21 +0200

    Project changes.

--- Lab_12/main.cpp
+++ Lab_12/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+class UniqueRng {
+private:
+public:
+    UniqueRng(unsigned int min, unsigned int max);
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int operator()();
+};
+
+void test_generator(UniqueRng ur, int count);
+
+int main(void) {
+    UniqueRng randGen(5, 13);
+
+    test_generator(randGen, 6);
+    test_generator(randGen, 9);
+    test_generator(randGen, 13);
+
+    UniqueRng randGen2(1, 35);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 70);
+
     return 0;
 }
