b4dc2d5097db77d466fa1f20936241f6463239e5
Christopher Romano <christopher.romano@icloud.com>
2021-12-19 02:20:27 +0200

    Finished Lab 12.

--- Lab_12/main.cpp
+++ Lab_12/main.cpp
@@ -1,16 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+
+// Class UniqueRng -----------------------------------------------------------------------------------------------------
+
 class UniqueRng {
 private:
+    vector<int> numbers;
+    int min;
+    int max;
 public:
-    UniqueRng(unsigned int min, unsigned int max);
+    UniqueRng(int min_ = 1, int max_ = 10) : numbers(), min(min_), max(max_) { srand(time(nullptr)); }
 
-    int operator()();
+    int operator()() {
+        int number;
+        do {
+            if (numbers.size() == (max - min + 1)) throw runtime_error("Unable to produce unique random number");
+            number = rand() % (max - min + 1) + min;
+        } while (find(numbers.begin(), numbers.end(), number) != numbers.end());
+        numbers.push_back(number);
+        return number;
+    };
 };
+
+
+// Main ----------------------------------------------------------------------------------------------------------------
 
 void test_generator(UniqueRng ur, int count);
 
-int main(void) {
+int main() {
     UniqueRng randGen(5, 13);
-
     test_generator(randGen, 6);
     test_generator(randGen, 9);
     test_generator(randGen, 13);
@@ -22,4 +46,20 @@ int main(void) {
     test_generator(randGen2, 70);
 
     return 0;
+}
+
+void test_generator(UniqueRng ur, int count) {
+    vector<int> vect;
+    cout << "Generating numbers:" << endl;
+    for (int i = 0; i < count; i++) {
+        try {
+            vect.push_back(ur());
+        } catch (const runtime_error &e) {
+            cout << "Exception: " << e.what() << endl;
+            cout << "Tried to generate " << count << " numbers. Got only " << i << endl;
+            break;
+        }
+    }
+    for (auto item: vect) cout << item << endl;
+    cout << "End of generator" << endl;
 }
